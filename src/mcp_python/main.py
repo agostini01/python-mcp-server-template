2d6b3598b419ace0d536a7a8dea981c7e9ee3abb
Nicolas Agostini <n.b.agostini@gmail.com>
2025-05-28 06:06:45 +0000

    Fix pylint warnings

--- src/mcp_python/main.py
+++ src/mcp_python/main.py
@@ -1,12 +1,19 @@
-from fastmcp import FastMCP
+"""Main module for MCP Python project.
+
+This module defines tools for MCP and runs the MCP server.
+"""
+
 import requests
+from fastmcp import FastMCP
 
 mcp = FastMCP("Demo 🚀")
+
 
 @mcp.tool()
 def add(a: int, b: int) -> int:
     """Add two numbers"""
     return a + b
+
 
 @mcp.tool()
 def fetch_gitignore_template(template: str) -> str:
@@ -22,22 +29,29 @@ def fetch_gitignore_template(template: str) -> str:
     # First retrieve the list of available templates and check if the template exists
     url = "https://www.toptal.com/developers/gitignore/api/list"
     params = {"format": "lines"}
-    response = requests.get(url, params=params)
+    response = requests.get(url, params=params, timeout=10)
     response.raise_for_status()
     # Reponse has this format: vcpkg,venv,vercel,vertx,video\nvim,virtualenv,...
     response = response.text.splitlines()
-    response = set(item.strip().lower() for line in response for item in line.split(","))
+    response = set(
+        item.strip().lower() for line in response for item in line.split(",")
+    )
     available_templates = response
 
     template = template.lower()
 
     if template not in available_templates:
-        raise ValueError(f"Template '{template}' not found in available templates.")
+        raise ValueError(
+            f"Template '{template}' not found in available templates."
+        )
 
     url = f"https://www.toptal.com/developers/gitignore/api/{template}"
-    response = requests.get(url)
+    response = requests.get(url, timeout=10)
     response.raise_for_status()
     return response.text
 
+
 if __name__ == "__main__":
-    mcp.run(transport="streamable-http", host="localhost", port=8000, path="/mcp/")
+    mcp.run(
+        transport="streamable-http", host="localhost", port=8000, path="/mcp/"
+    )
